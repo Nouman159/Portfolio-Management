a91f9b5c9ac036f9e79f34aef780b66cab322ab9
Nouman Arshad <noumanarshad15926@gmail.com>
2024-08-11 14:56:47 +0500

    Update

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import Testimonials from '../Components/Testimonials'
+import Projects from '../Components/Projects'
 
 const Home = () => {
     return (
         <div>
             <Hero />
             <Testimonials />
+            <Projects />
         </div>
     )
 }
