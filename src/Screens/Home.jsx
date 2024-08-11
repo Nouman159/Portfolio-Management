4c81d4284895ac6926a684b39487e39d9759dc48
BilalMustafa123 <mbm15486@gmail.com>
2024-08-11 16:24:58 +0500

    projects added

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -8,9 +8,14 @@ const Home = () => {
     return (
         <div>
             <Hero />
+            <ProjectDetails />
             <Testimonials />
             <Projects />
+<<<<<<< Updated upstream
             <ProjectDetails title="Project details" details={["In today's rapidly evolving digital landscape, staying ahead of the curve requires a blend of creativity and technological expertise. This project is a testament to our commitment to pushing the boundaries of web development. By integrating cutting-edge technologies with user-centric design, we've crafted a solution that not only meets current demands but also anticipates future trends.", "As the digital world continues to expand, the importance of seamless and intuitive user experiences cannot be overstated. This project was conceived with the goal of delivering a platform that is both visually appealing and functionally robust. Through a meticulous design process and the application of best practices in development, we've created a product that is set to redefine industry standards.", "As the digital world continues to expand, the importance of seamless and intuitive user experiences cannot be overstated. This project was conceived with the goal of delivering a platform that is both visually appealing and functionally robust. Through a meticulous design process and the application of best practices in development, we've created a product that is set to redefine industry standards.", "As the digital world continues to expand, the importance of seamless and intuitive user experiences cannot be overstated. This project was conceived with the goal of delivering a platform that is both visually appealing and functionally robust. Through a meticulous design process and the application of best practices in development, we've created a product that is set to redefine industry standards.", "As the digital world continues to expand, the importance of seamless and intuitive user experiences cannot be overstated. This project was conceived with the goal of delivering a platform that is both visually appealing and functionally robust. Through a meticulous design process and the application of best practices in development, we've created a product that is set to redefine industry standards."]} images={["/assets/download.jfif", "/assets/download.jfif", "/assets/download.jfif", "/assets/download.jfif", "/assets/download.jfif"]} />
+=======
+
+>>>>>>> Stashed changes
         </div>
     )
 }
